a1f837b55565ae24b7adec4409b71e20cbc383ec
crystian97 <icrystianveloso@gmail.com>
2022-11-25 15:22:40 -0300

    refactor:removing component not necessary

--- ignite-timer/src/App.tsx
+++ ignite-timer/src/App.tsx
@@ -1,12 +1,14 @@
-import { Button } from './components/Button'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import {BrowserRouter} from 'react-router-dom'
+import { Router } from './Router'
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
