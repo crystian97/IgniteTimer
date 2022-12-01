669acead98143cfcdd86197346fb4ea051feec16
crystian97 <icrystianveloso@gmail.com>
2022-12-01 20:48:23 -0300

    feat:history list using status color

--- ignite-timer/src/pages/History/index.tsx
+++ ignite-timer/src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
 return(
@@ -19,49 +19,49 @@ return(
                         <td>Tarefa</td>
                         <td>20 Minutos</td>
                         <td>Há 2 meses</td>
-                        <td>Concluído</td>
+                        <td><Status statusColor="green">Concluído</Status></td>
                     </tr>
                     <tr>
                         <td>Tarefa</td>
                         <td>20 Minutos</td>
                         <td>Há 2 meses</td>
-                        <td>Concluído</td>
+                        <td><Status statusColor="green">Concluído</Status></td>
                     </tr>
                     <tr>
                         <td>Tarefa</td>
                         <td>20 Minutos</td>
                         <td>Há 2 meses</td>
-                        <td>Concluído</td>
+                        <td><Status statusColor="green">Concluído</Status></td>
                     </tr>
                     <tr>
                         <td>Tarefa</td>
                         <td>20 Minutos</td>
                         <td>Há 2 meses</td>
-                        <td>Concluído</td>
+                        <td><Status statusColor="green">Concluído</Status></td>
                     </tr>
                     <tr>
                         <td>Tarefa</td>
                         <td>20 Minutos</td>
                         <td>Há 2 meses</td>
-                        <td>Concluído</td>
+                        <td><Status statusColor="green">Concluído</Status></td>
                     </tr>
                     <tr>
                         <td>Tarefa</td>
                         <td>20 Minutos</td>
                         <td>Há 2 meses</td>
-                        <td>Concluído</td>
+                        <td><Status statusColor="green">Concluído</Status></td>
                     </tr>
                     <tr>
                         <td>Tarefa</td>
                         <td>20 Minutos</td>
                         <td>Há 2 meses</td>
-                        <td>Concluído</td>
+                        <td><Status statusColor="green">Concluído</Status></td>
                     </tr>
                     <tr>
                         <td>Tarefa</td>
                         <td>20 Minutos</td>
                         <td>Há 2 meses</td>
-                        <td>Concluído</td>
+                        <td><Status statusColor="green">Concluído</Status></td>
                     </tr>
                 </tbody>
             </table>
