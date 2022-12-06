2d348f673ccd6488b04162aa634fb6ad002d2158
crystian97 <icrystianveloso@gmail.com>
2022-12-05 21:50:31 -0300

    feat:change title page to timer

--- ignite-timer/src/pages/Home/index.tsx
+++ ignite-timer/src/pages/Home/index.tsx
@@ -49,6 +49,10 @@ export function Home() {
   const secondsAmount = currentSeconds %60
   const minutes = String(minutesAmount).padStart(2,'0')
   const  seconds = String(secondsAmount).padStart(2,'0')
+  useEffect(()=>{
+    if(activeCycle)
+      document.title= `${minutes}:${seconds}`
+  },[minutes,seconds,activeCycle])
   const task = watch('task')
   const isSubmitDisabled = !task
   useEffect(()=>{
