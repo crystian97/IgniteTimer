fcf4f00d006f6ae75303aa669f9bc11e89e4ee7a
crystian97 <icrystianveloso@gmail.com>
2022-12-14 21:02:43 -0300

    refactor: separate actions

--- ignite-timer/src/contexts/CyclesContext.tsx
+++ ignite-timer/src/contexts/CyclesContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useState, useReducer  } from "react";
-import { ActionTypes, Cycle, cyclesReducer } from "../reducers/cycles";
+import {  Cycle, cyclesReducer } from "../reducers/cycles/reducer";
+import { ActionTypes, addNewCycleAction, interruptedDateAction, markCurrentCycleAsFinishedAction } from "./actions";
 interface CreateCycleData{
     task:string
     minutesAmount:number
@@ -38,13 +39,7 @@ export function CyclesContextProvider({children}:CycleContextProviderProps) {
     setAmountSecondsPassed(seconds);
   }
   function markCurrentCycleAsFinished() {
-
-    dispatch({
-      type:ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
-      payload:{
-        activeCycleId
-      },
-    })
+    dispatch(markCurrentCycleAsFinishedAction())
   }
   function createNewCycle(data: CreateCycleData) {
     const id = String(new Date().getTime());
@@ -54,24 +49,14 @@ export function CyclesContextProvider({children}:CycleContextProviderProps) {
       minutesAmount: data.minutesAmount,
       startDate: new Date(),
     };
-    dispatch({
-      type:ActionTypes.ADD_NEW_CYCLE,
-      payload:{
-        newCycle,
-      },
-    })
+    dispatch(addNewCycleAction(newCycle))
     setAmountSecondsPassed(0);
     
   }
 
   function interruptCurrentCycle() {
   
-    dispatch({
-      type:ActionTypes.INTERRUPT_CURRENT_CYCLE,
-      payload:{
-        activeCycleId,
-      },
-    })
+    dispatch(interruptedDateAction())
   }
   return(
   <CyclesContext.Provider
