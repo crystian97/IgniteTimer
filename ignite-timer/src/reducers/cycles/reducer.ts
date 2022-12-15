12fa78f72f6d4aeadeb48c3c7c5b1a8279d72f03
crystian97 <icrystianveloso@gmail.com>
2022-12-14 22:55:10 -0300

    feat:using immerjs

--- ignite-timer/src/reducers/cycles/reducer.ts
+++ ignite-timer/src/reducers/cycles/reducer.ts
@@ -1,5 +1,5 @@
 import { ActionTypes } from "../../contexts/actions";
-
+import {produce}  from 'immer'
 export interface Cycle {
     id: string;
     task: string;
@@ -14,38 +14,39 @@ activeCycleId:string|null
 }
 
 export function cyclesReducer(state:CyclesState,action:any){
-
         switch(action.type){
           case ActionTypes.ADD_NEW_CYCLE:
-            return {
-              ...state,
-              cycles:[...state.cycles,action.payload.newCycle],
-              activeCycleId:action.payload.newCycle.id
-            }
+         
+            return produce(state,draft=>{
+              draft.cycles.push(action.payload.newCycle)
+              draft.activeCycleId= action.payload.newCycle.id
+            })
           case ActionTypes.INTERRUPT_CURRENT_CYCLE:
-            return {
-              ...state,
-              cycles:state.cycles.map((cycle) => {
-                    if (cycle.id === state.activeCycleId) {
-                      return { ...cycle, interruptedDate: new Date() };
-                    } else {
-                      return cycle;
-                    }
-                  }),
-              activeCycleId:null
+
+            const currentCycleIndex = state.cycles.findIndex(cycle=>{
+              return cycle.id === state.activeCycleId
+            })
+            if(currentCycleIndex <0){
+              return state;
             }
+            return produce(state,draft=>{
+             
+              draft.activeCycleId=null
+              draft.cycles[currentCycleIndex].interruptedDate= new Date()
+            })
           case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
-          return {
-            ...state,
-            cycles:state.cycles.map((cycle) => {
-                  if (cycle.id === state.activeCycleId) {
-                    return { ...cycle, finishedDate: new Date() };
-                  } else {
-                    return cycle;
-                  }
-                }),
-            activeCycleId:null
+         
+          const currentCycleIndexFinished = state.cycles.findIndex(cycle=>{
+            return cycle.id === state.activeCycleId
+          })
+          if(currentCycleIndexFinished <0){
+            return state;
           }
+          return produce(state,draft=>{
+           
+            draft.activeCycleId=null
+            draft.cycles[currentCycleIndexFinished].finishedDate= new Date()
+          })
           default:
             return state
         }
